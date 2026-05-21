d89510e80393142997a276a61e43e216b71e5b1f
孫健太 <45735033+sxd19911008@users.noreply.github.com>
2026-05-21 12:10:31 +0800

    feat: jexl插件包清理lombok

--- step-flow-handler-jexl/src/main/java/io/github/kentasun/stepflow/jexl/dto/JexlStepHandlerProperties.java
+++ step-flow-handler-jexl/src/main/java/io/github/kentasun/stepflow/jexl/dto/JexlStepHandlerProperties.java
@@ -1,17 +1,8 @@
 package io.github.kentasun.stepflow.jexl.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 /**
  * 表达式引擎通用配置项
  */
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class JexlStepHandlerProperties {
 
     /**
@@ -23,4 +14,62 @@ public class JexlStepHandlerProperties {
      * 是否开启引擎调试日志
      */
     private Boolean debug;
+
+    public JexlStepHandlerProperties(Integer cache, Boolean debug) {
+        this.cache = cache;
+        this.debug = debug;
+    }
+
+    public JexlStepHandlerProperties() {
+    }
+
+    public Integer getCache() {
+        return this.cache;
+    }
+
+    public Boolean getDebug() {
+        return this.debug;
+    }
+
+    public void setCache(Integer cache) {
+        this.cache = cache;
+    }
+
+    public void setDebug(Boolean debug) {
+        this.debug = debug;
+    }
+
+    public String toString() {
+        return "JexlStepHandlerProperties(cache=" + this.getCache() + ", debug=" + this.getDebug() + ")";
+    }
+
+    public static JexlStepHandlerPropertiesBuilder builder() {
+        return new JexlStepHandlerPropertiesBuilder();
+    }
+
+    public static class JexlStepHandlerPropertiesBuilder {
+        private Integer cache;
+        private Boolean debug;
+
+        JexlStepHandlerPropertiesBuilder() {
+        }
+
+        public JexlStepHandlerPropertiesBuilder cache(Integer cache) {
+            this.cache = cache;
+            return this;
+        }
+
+        public JexlStepHandlerPropertiesBuilder debug(Boolean debug) {
+            this.debug = debug;
+            return this;
+        }
+
+        public JexlStepHandlerProperties build() {
+            return new JexlStepHandlerProperties(this.cache, this.debug);
+        }
+
+        public String toString() {
+            return "JexlStepHandlerProperties.JexlStepHandlerPropertiesBuilder(cache=" + this.cache + ", debug=" + this.debug + ")";
+        }
+    }
 }
--- step-flow-handler-jexl/src/test/java/io/github/kentasun/stepflow/dto/CalcDTO.java
+++ step-flow-handler-jexl/src/test/java/io/github/kentasun/stepflow/dto/CalcDTO.java
@@ -1,20 +1,99 @@
 package io.github.kentasun.stepflow.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Data
 public class CalcDTO {
 
     private BigDecimal num1;
     private BigDecimal num2;
     private BigDecimal num3;
     private BigDecimal num4;
+
+    public CalcDTO(BigDecimal num1, BigDecimal num2, BigDecimal num3, BigDecimal num4) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.num3 = num3;
+        this.num4 = num4;
+    }
+
+    public CalcDTO() {
+    }
+
+    public BigDecimal getNum1() {
+        return this.num1;
+    }
+
+    public BigDecimal getNum2() {
+        return this.num2;
+    }
+
+    public BigDecimal getNum3() {
+        return this.num3;
+    }
+
+    public BigDecimal getNum4() {
+        return this.num4;
+    }
+
+    public void setNum1(BigDecimal num1) {
+        this.num1 = num1;
+    }
+
+    public void setNum2(BigDecimal num2) {
+        this.num2 = num2;
+    }
+
+    public void setNum3(BigDecimal num3) {
+        this.num3 = num3;
+    }
+
+    public void setNum4(BigDecimal num4) {
+        this.num4 = num4;
+    }
+
+    public String toString() {
+        return "CalcDTO(num1=" + this.getNum1() + ", num2=" + this.getNum2() + ", num3=" + this.getNum3() + ", num4=" + this.getNum4() + ")";
+    }
+
+    public static CalcDTOBuilder builder() {
+        return new CalcDTOBuilder();
+    }
+
+    public static class CalcDTOBuilder {
+        private BigDecimal num1;
+        private BigDecimal num2;
+        private BigDecimal num3;
+        private BigDecimal num4;
+
+        CalcDTOBuilder() {
+        }
+
+        public CalcDTOBuilder num1(BigDecimal num1) {
+            this.num1 = num1;
+            return this;
+        }
+
+        public CalcDTOBuilder num2(BigDecimal num2) {
+            this.num2 = num2;
+            return this;
+        }
+
+        public CalcDTOBuilder num3(BigDecimal num3) {
+            this.num3 = num3;
+            return this;
+        }
+
+        public CalcDTOBuilder num4(BigDecimal num4) {
+            this.num4 = num4;
+            return this;
+        }
+
+        public CalcDTO build() {
+            return new CalcDTO(this.num1, this.num2, this.num3, this.num4);
+        }
+
+        public String toString() {
+            return "CalcDTO.CalcDTOBuilder(num1=" + this.num1 + ", num2=" + this.num2 + ", num3=" + this.num3 + ", num4=" + this.num4 + ")";
+        }
+    }
 }
